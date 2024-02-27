d3f383232d77887510b0809f7311ecc1fdb8355d
Usuario <Usuario@CPD02-M12>
2024-02-27 11:28:54 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a sua idade em anos: ");
+            int anos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual sua idade em meses: ");
+            int meses= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual suas idade em dias: ");
+            int dias = int.Parse(Console.ReadLine());
+
+            int resultado = (anos*365)+(meses*30)+ dias;
+
+            Console.WriteLine("A sua idade total em dias é: " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
